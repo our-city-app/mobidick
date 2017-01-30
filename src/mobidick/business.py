ddcde7448bfc024e4fb81e37f336f3fa871db68d
Bart Pede <bart@mobicage.com>
2017-01-30 15:46:59 +0200

    Regenerated license headers

--- src/mobidick/business.py
+++ src/mobidick/business.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Mobicage NV
+# Copyright 2017 Mobicage NV
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# @@license_version:1.1@@
+# @@license_version:1.2@@
 
 import base64
 import datetime
